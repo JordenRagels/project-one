38bf40b45a3b868d8afab9a49bd1f6fff869d96f
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-25 10:07:20 -0600

    Writes test to ensure Cypress is integrated into the project

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,21 @@
+
+describe('Simple Test', () => {
+    it('Tests that Cypress is integrated', () => {
+        expect(true).to.equal(true) // sample passing test
+        // expect(true).to.equal(false) // sample failing test
+    })
+})
+
+// (https://docs.cypress.io/guides/getting-started/writing-your-first-test.html#Write-a-real-test)
+// 3 phases to testing application:
+    // 1) Set up the application state.
+    // 2) Take an action.
+    // 3) Make an assertion about the resulting application state.
+
+describe('QA Tests for Project One', () => {
+    it('Visits the Rhythm Radar Page', () => {
+        cy.visit('localhost:5500/index.html')
+    })
+})
+
+
